3507c852054e03019c50852a6a04218e39befaa7
badasswp <badasswpdev@gmail.com>
2024-04-28 14:40:05 +0100

    chore: test that options return default settings

--- tests/WebPImageConverterTest.php
+++ tests/WebPImageConverterTest.php
@@ -17,4 +17,30 @@ class WebPImageConverterTest extends TestCase {
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_get_options_returns_default_settings() {
+		$converter = Mockery::mock( WebPImageConverter::class )->makePartial();
+		$converter->shouldAllowMockingProtectedMethods();
+
+		\WP_Mock::expectFilter(
+			'webp_img_options',
+			[
+				'quality'     => 20,
+				'max-quality' => 100,
+				'converter'   => 'gd',
+			]
+		);
+
+		$options = $converter->get_options();
+
+		$this->assertSame(
+			$options,
+			[
+				'quality'     => 20,
+				'max-quality' => 100,
+				'converter'   => 'gd',
+			]
+		);
+		$this->assertConditionsMet();
+	}
 }
