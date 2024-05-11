1da1154d5c4679d997883ebbc9cb9a11151cae23
badasswp <badasswpdev@gmail.com>
2024-05-11 07:56:19 +0100

    test: fix failing tests

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -40,6 +40,7 @@ class PluginTest extends TestCase {
 		\WP_Mock::expectFilterAdded( 'wp_get_attachment_image', [ $this->instance, 'filter_wp_get_attachment_image' ], 10, 5 );
 		\WP_Mock::expectFilterAdded( 'post_thumbnail_html', [ $this->instance, 'filter_post_thumbnail_html' ], 10, 5 );
 		\WP_Mock::expectFilterAdded( 'wp_generate_attachment_metadata', [ $this->instance, 'generate_webp_srcset_images' ], 10, 3 );
+		\WP_Mock::expectActionAdded( 'delete_attachment', [ $this->instance, 'remove_webp_images' ] );
 
 		$this->instance->run();
 
