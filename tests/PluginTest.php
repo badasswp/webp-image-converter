346c78df1b764d16bf8edde286e6ab300348dea2
badasswp <badasswpdev@gmail.com>
2024-05-16 08:51:20 +0100

    test: add utility methods to help with tests

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -240,4 +240,22 @@ class PluginTest extends TestCase {
 
 		$this->assertConditionsMet();
 	}
+
+	public function create_mock_image( $image_file_name ) {
+		// Create a blank image.
+		$width  = 400;
+		$height = 200;
+		$image  = imagecreatetruecolor( $width, $height );
+
+		// Set background color.
+		$bg_color = imagecolorallocate( $image, 255, 255, 255 );
+		imagefill( $image, 0, 0, $bg_color );
+		imagejpeg( $image, $image_file_name );
+	}
+
+	public function destroy_mock_image( $image_file_name ) {
+		if ( file_exists( $image_file_name ) ) {
+			unlink( $image_file_name );
+		}
+	}
 }
